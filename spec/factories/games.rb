c287f18e066b05b9288a766ad53243089e53aa59
rickutino <rickutino@gmail.com>
2020-11-28 06:29:22 +0900

    add game model test.

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :game do
-    mode { 1 }
+    mode { %i(pvp pve both).sample }
     release_date { "2020-11-27 06:35:41" }
-    developer { "MyString" }
-    system_requirement { nil }
+    developer { Faker::Company.name }
+    system_requirement
   end
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,11 @@ FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
-    price { Feker::commerce::Price(range: 5000..9900) }
-    # productable { nil }
+    price { Faker::Commerce::price(range: 5000..9900) }
+    
+    after :build do |product|
+      product.productable = create(:game)
+    end
+
   end
 end
