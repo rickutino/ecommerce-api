23299fe8a3b187c3c0286c57f72da2e9a8e1d319
rickutino <rickutino@gmail.com>
2020-11-28 18:23:45 +0900

    テストの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
-require 'rails-helper'
+require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_prasence_of(:profile) }
-  it { is_expected.to define_enum_for(:profile).with_values({( admin: 0, client: 1 )})}
+  it { is_expected.to validate_presence_of(:profile) }
+  it { is_expected.to define_enum_for(:profile).with_values({ admin: 0, client: 1 })}
 end
