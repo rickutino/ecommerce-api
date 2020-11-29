1220eab237232cb01825c3dac375bb52c97f2475
rickutino <rickutino@gmail.com>
2020-11-29 16:47:31 +0900

    coupon validation setting

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive } #nao importa se e caixa alta ou nao.
 
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
