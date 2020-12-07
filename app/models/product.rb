8f7c4c7b1674a9ed7cbfb053368ce3b2e77def21
Ricardo Utino <rickutino@gmail.com>
2020-12-07 03:57:35 +0000

    ModelでのPaginatable機能

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,7 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :image, presence: true
+
+  include NameSearchable
+  include Paginatable
 end
