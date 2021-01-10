3dee2c636a2b2d03a15a713127e48e0bc5c5e4bd
rickutino <rickutino@gmail.com>
2021-01-10 19:43:24 +0900

    add MVP coupons POST action

--- app/controllers/admin/v1/coupons_controller.rb
+++ app/controllers/admin/v1/coupons_controller.rb
@@ -3,5 +3,22 @@ module Admin::V1
     def index
       @coupons = Coupon.all
     end
+
+    def create
+      @coupon = Coupon.new
+      @coupon.attributes = coupon_params
+
+      @coupon.save!
+      render :show
+    rescue
+      render json: { errors: { fields: @coupon.errors.messages } }, status: :unprocessable_entity
+    end
+
+    private
+
+    def coupon_params
+      return {} unless params.has_key?(:coupon)
+      params.require(:coupon).permit(:id, :name, :code, :status, :discount_value, :max_use, :due_date)
+    end
   end
 end
