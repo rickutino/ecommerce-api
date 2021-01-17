04b47f525f1be69be8ec7185fbe8647231bc6091
rickutino <rickutino@gmail.com>
2021-01-17 16:45:43 +0900

    add User post  action MVP

--- app/controllers/admin/v1/users_controller.rb
+++ app/controllers/admin/v1/users_controller.rb
@@ -3,5 +3,25 @@ module Admin::V1
     def index
       @users = User.where.not(id: @current_user.id)
     end
+
+    def create
+      @user = User.new
+      @user.attributes = user_params
+      save_user!
+    end
+
+    private
+
+    def user_params
+      return {} unless params.has_key?(:user)
+      params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :profile)
+    end
+
+    def save_user!
+      @user.save!
+      render :show
+    rescue
+      render_error(fields: @user.errors.messages)
+    end
   end
 end
--- spec/requests/admin/v1/users_request_spec.rb
+++ spec/requests/admin/v1/users_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::V1::Users", type: :request do
-  let(:user) { create(:user) }
+  let!(:user) { create(:user) }
 
   context "GET /admin/v1/users" do
     let(:url) { "/admin/v1/users" }
@@ -17,4 +17,48 @@ RSpec.describe "Admin::V1::Users", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  context "POST /admin/v1/users" do
+    let(:url) { "/admin/v1/users" }
+
+    context "with valida params" do
+      let!("user_params") { { user: attributes_for(:user) }.to_json }
+
+      it "adds a new User" do
+        expect do
+          post url, headers: auth_header(user), params: user_params
+        end.to change(User, :count).by(1)
+      end
+      it "returns last adds user" do
+        post url, headers: auth_header(user),params: user_params
+        expected_user = User.last.as_json( only: %i(id name email profile) )
+        expect(body_json['user']).to eq expected_user
+      end
+      it "returns ok status" do
+        post url, headers: auth_header(user), params: user_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "with invalid params" do 
+      let!(:invalid_user_params) { { user: attributes_for(:user, name: nil) }.to_json }
+
+      it "does not add a new User" do
+        expect do
+          post url, headers: auth_header(user), params: invalid_user_params
+        end.to_not change(User, :count)
+      end
+
+      it "returns errors messages" do
+        post url, headers: auth_header, params: invalid_user_params
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it "returns errors messages" do
+        post url, headers: auth_header, params: invalid_user_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+  
 end
